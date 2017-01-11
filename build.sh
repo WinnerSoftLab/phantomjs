ab129bc3bbbf629aa88f070e96ce691b5b296f20
lispad <lispad@gmail.com>
2017-01-11 14:21:07 +0300

    ITCT-1095 Add file download events

--- build.sh
+++ build.sh
@@ -10,10 +10,22 @@ printf "Building ariel without touch support. QtBase and QtWebkit will be rebuil
 patch -f -i notouch.patch src/qt/qtwebkit/Tools/qmake/mkspecs/features/features.pri
 
 python build.py --git-clean-qtbase --git-clean-qtwebkit --qt-config "-D ENABLE_TOUCH_EVENTS=0" --release --confirm
-mv bin/phantomjs bin/arieljs-notouch || echo "Build no touch failed" && exit 1
+if [ -f bin/phantomjs ];
+then
+   mv bin/phantomjs bin/arieljs-notouch
+else
+   echo "Build no touch failed"
+   exit 1
+fi
 
 printf "Building ariel with touch support. QtBase and QtWebkit will be rebuilt\n\n"
 patch -fR -i notouch.patch src/qt/qtwebkit/Tools/qmake/mkspecs/features/features.pri
 make clean
 python build.py --git-clean-qtbase --git-clean-qtwebkit --release --confirm
-mv bin/phantomjs bin/arieljs || echo "Build touch failed" && exit 1
+if [ -f bin/phantomjs ];
+then
+   mv bin/phantomjs bin/arieljs
+else
+   echo "Build touch failed"
+   exit 1
+fi
