4bf6b5b1764d90a86628d5f09e28d3172f8fe317
lispad <lispad@gmail.com>
2017-01-11 13:18:24 +0300

    ITCT-1095 Add file download events

--- build.sh
+++ build.sh
@@ -2,17 +2,18 @@
 printf "Two versions of arieljs will be build:\n-) with touch support (bin/arieljs)\n-) without (bin/arieljs-notouch)\n"
 
 printf "Removing old binaries\n\n"
+rm -f bin/phantomjs
 rm -f bin/arieljs
 rm -f bin/arieljs-notouch
 
 printf "Building ariel without touch support. QtBase and QtWebkit will be rebuilt\n\n"
 patch -f -i notouch.patch src/qt/qtwebkit/Tools/qmake/mkspecs/features/features.pri
 
-/usr/bin/python build.py --git-clean-qtbase --git-clean-qtwebkit --qt-config "-D ENABLE_TOUCH_EVENTS=0" --webkit-qmake-args "-D ENABLE_TOUCH_EVENTS=0" --release --confirm
-mv bin/phantomjs bin/arieljs-notouch
+python build.py --git-clean-qtbase --git-clean-qtwebkit --qt-config "-D ENABLE_TOUCH_EVENTS=0" --release --confirm
+mv bin/phantomjs bin/arieljs-notouch || echo "Build no touch failed" && exit 1
 
 printf "Building ariel with touch support. QtBase and QtWebkit will be rebuilt\n\n"
 patch -fR -i notouch.patch src/qt/qtwebkit/Tools/qmake/mkspecs/features/features.pri
 make clean
 python build.py --git-clean-qtbase --git-clean-qtwebkit --release --confirm
-mv bin/phantomjs bin/arieljs
+mv bin/phantomjs bin/arieljs || echo "Build touch failed" && exit 1
